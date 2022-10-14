0c6e9c3b531311a48c1cdcf1f57c7317dbdb8ef4
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-10-14 12:28:54 +0300

    first lesson testsql

---
+++ testsql/testsql.js
@@ -0,0 +1,18 @@
+const mysql= require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password :'',
+    database: 'Dibya',
+});
+
+connection.connect();
+
+connection.query(`select * from tasks`, (error, results, fields)=>{
+if (error){
+    return console.log(error);
+}
+
+return console.log(results);
+});
+connection.end();
