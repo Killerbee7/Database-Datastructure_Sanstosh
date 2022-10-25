956fe2166e905419d8803ff84f72eec5217bdf83
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-10-25 09:27:44 +0300

    practice updates

---
+++ Practice_2410/practice/anagram.js
@@ -0,0 +1,29 @@
+//Given two strings, write a function to determine if the second string is an anagram
+//of the first.
+
+//An anagram is a word, phrase, or name formed by rearranging the letters of another,
+//such as cinema, formed from iceman.
+
+// Time complexity - O(n)
+
+validAnagram('', '') // true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') // true
+validAnagram("rat","car") // false) // false
+
+
+
+function validAnagram(stringA, stringB){
+
+    // if(stringA.trim()==stringB.trim()){
+    //     return ok
+    // }
+    // else{
+    //     return false
+    // }
+
+}
+
+// function cleanString(string){
+
+// }
